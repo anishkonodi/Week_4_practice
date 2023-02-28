0415a863496ac97c58897125b040410eae415ee8
Anish Bharadwaj K G <124552306+anishkonodi@users.noreply.github.com>
2023-02-28 21:36:02 +0530

    Update Encryption.java

--- day17/Encryption.java
+++ day17/Encryption.java
@@ -2,33 +2,7 @@ class Encryption {
   public static void main(String args[]) {
     String str = "Hello Welcome";
     System.out.println("Before encryption string is : " + str);
-    // char ch[] = new char[str.length()];
-    // for (int i = 0; i < str.length(); i++) {
-    // ch[i] = str.charAt(i);
-    // }
-    // String st = " ";
-    // for (int i = 0; i < ch.length; i++) {
-    // // if (ch[i] != ' ') {
-    // if (i % 2 == 0) {
-    // st = st + ch[i];
-    // }
-    // // }
-    // }
-    // for (int i = 0; i < ch.length; i++) {
-    // // if (ch[i] != ' ') {
-    // if (i % 2 == 1) {
-    // st = st + ch[i];
-    // }
-    // // }
-    // }
-    // System.out.println("Encrypted String is : " + st);
-    // char c[] = new char[st.length()];
-    // for (int i = 0; i < st.length(); i++) {
-    // c[i] = st.charAt(i);
-    // }
-    // for (int i = 0; i < c.length; i++) {
-    // if()
-    // }
+
     System.out.println("After encryption : ");
     for (int i = 0; i < str.length(); i++) {
       if (str.charAt(i) != ' ') {
